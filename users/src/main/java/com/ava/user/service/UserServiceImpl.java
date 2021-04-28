c50f23e7d6f314d9a03c835aeeb2248c27a546e6
Avaneesh Yadav <avaneesh.yadav@oracle.com>
2021-04-28 15:44:26 +0530

    removing all the unused imports and propertes

--- users/src/main/java/com/ava/user/service/UserServiceImpl.java
+++ users/src/main/java/com/ava/user/service/UserServiceImpl.java
@@ -3,7 +3,6 @@ package com.ava.user.service;
 import com.ava.user.dao.UserRepository;
 import com.ava.user.entity.Users;
 
-import org.apache.commons.logging.LogFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
